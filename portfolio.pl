f834ffbc3420c69fef1938e20fdf7a655b54303a
Qingyuan Zhang <miccrun@gmail.com>
2013-11-10 14:48:59 -0600

    add foo function, for testing

--- portfolio.pl
+++ portfolio.pl
@@ -31,11 +31,13 @@ use Time::CTime;
 use Date::Manip;
 use Finance::QuoteHist::Yahoo;
 
+sub foo {
+    print "a";
+}
 
-#print"Content-type: text/html\n\n";
 
 
-# The session cookie will contain the user's name and password so that
+# The    new   session cookie will contain the user's name and password so that
 # he doesn't have to type it again and again.
 #
 # "PortfolioSession"=>"user/password"
