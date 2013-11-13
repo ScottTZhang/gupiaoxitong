102223dac931cddcf3469575cd808f0850290384
Qingyuan Zhang <miccrun@gmail.com>
2013-11-13 15:27:13 -0600

    Make daily_update.sh executable

--- daily_update.sh
+++ daily_update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl "http://murphy.wot.eecs.northwestern.edu/~qzw056/portfolio/portfolio.pl?act=update-daily-stocks&debug=1" >/dev/null 2>&1
+curl "http://murphy.wot.eecs.northwestern.edu/~qzw056/portfolio/portfolio.pl?act=update-daily-stocks&debug=1" >/dev/null 2>&1 &
